125ea5476445f307033b2549adb2873c90b26a49
churley24 <116841602+churley24@users.noreply.github.com>
2022-10-29 15:04:49 -0400

    Add files via upload

---
+++ L91.py
@@ -0,0 +1,8 @@
+#Camryn and Britney
+
+def breakify(lines):
+    return "<br>".join(lines)
+
+lines = ["Haiku frogs in snow",
+        "A limerick came from Nantucket",
+        "Tetrametric drum-beats thrumming, Hiawathianic rhythm."]
